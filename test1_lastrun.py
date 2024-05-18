d2c7b2d027d5365ec2e9668ed692d1ca5eb2aec5
Eli Liu <eliliu@suttongrammar.school>
2024-05-18 19:09:06 +0100

    test2

--- test1_lastrun.py
+++ test1_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.1),
-    on May 18, 2024, at 18:57
+    on May 18, 2024, at 19:04
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
