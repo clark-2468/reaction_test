05b2ede1586eb97508fae893f616672974b21acf
Eli Liu <eliliu@suttongrammar.school>
2024-07-02 15:31:29 +0100

    Boazdata

--- test1_lastrun.py
+++ test1_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.1),
-    on June 30, 2024, at 17:16
+    on July 01, 2024, at 15:51
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
